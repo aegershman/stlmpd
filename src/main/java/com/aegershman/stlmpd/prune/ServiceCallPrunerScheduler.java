2845744995b7821fd453f4200f1aaecc7d96f8c7
Aaron Gershman <aegershman@gmail.com>
2024-01-10 16:02:18 -0600

    formatting to make editor happy. spring boot config processor to appease ide. fix jpa property key, rm unused slf4j decl, format num long without underscore

--- src/main/java/com/aegershman/stlmpd/prune/ServiceCallPrunerScheduler.java
+++ src/main/java/com/aegershman/stlmpd/prune/ServiceCallPrunerScheduler.java
@@ -2,13 +2,11 @@ package com.aegershman.stlmpd.prune;
 
 import com.aegershman.stlmpd.api.ServiceCallService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
-@Slf4j
 public class ServiceCallPrunerScheduler {
     private final ServiceCallService serviceCallService;
 
