d8fd86f6992dee437120f79102cfd8036ddf7100
Aaron Gershman <aegershman@gmail.com>
2024-01-18 17:27:25 -0600

    Refactor geocoding service to be single-purposed, not worry about whether geocoding service api is defined

--- src/main/java/com/aegershman/stlmpd/geocode/GeocodingService.java
+++ src/main/java/com/aegershman/stlmpd/geocode/GeocodingService.java
@@ -1,13 +1,11 @@
 package com.aegershman.stlmpd.geocode;
 
 import com.aegershman.stlmpd.StlmpdProperties;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -18,41 +16,23 @@ import java.net.http.HttpResponse;
 public class GeocodingService {
 
     private final StlmpdProperties properties;
+    private final HttpClient client = HttpClient.newHttpClient();
+    private final ObjectMapper objectMapper = new ObjectMapper()
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
     public Position geocodeAddressToGPS(String streetAddress) {
-        String apiKey = properties.getGeocodeApiToken();
-        String URL = "%s/geocode?q=%s&apiKey=%s";
-        String address = streetAddress.replace(' ', '+');
-        String formattedURL = URL.formatted(properties.getGeocodeApiUrlBase(), address, apiKey);
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = null;
-        try {
-            request = HttpRequest.newBuilder().uri(URI.create(formattedURL)).build();
-        } catch (IllegalArgumentException e) {
-            // todo
-            return null;
-        }
-        HttpResponse<String> httpResponse = null;
-        try {
-            httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        String formattedURL = String.format("%s/geocode?q=%s&apiKey=%s",
+                properties.getGeocodeApiUrlBase(),
+                streetAddress.replace(' ', '+'),
+                properties.getGeocodeApiToken());
 
-        String body = httpResponse.body();
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         try {
-            Items items = objectMapper.readValue(body, Items.class);
-            if (items == null || items.getItems() == null) {
-                // todo
-                return null;
-            } else {
-                return items.getItems().get(0).getPosition();
-            }
-        } catch (JsonProcessingException e) {
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(formattedURL)).build();
+            HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+            Items items = objectMapper.readValue(httpResponse.body(), Items.class);
+
+            return (items != null && items.getItems() != null) ? items.getItems().get(0).getPosition() : null;
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
